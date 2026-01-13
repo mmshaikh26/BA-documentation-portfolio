b8ce281ed9a6330422af0e2d2610582e26427a3a
Maruf Shaikh <136592174+mmshaikh26@users.noreply.github.com>
2026-01-13 17:25:38 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  // IMPORTANT: set this to "/BA-documentation-portfolio/" (or match your repo name)
+  // IMPORTANT: set this to "/BA-documentation-portfolio/" to match your repo name on GitHub Pages
   base: '/BA-documentation-portfolio/',
   plugins: [react()],
 })
